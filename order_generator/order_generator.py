7598e40193efa935f535d723ab8ea4c7d98104ed
Neya <neyshivsen08@gmail.com>
2026-08-16 21:06:50 +0530

    Secure database credentials

--- order_generator/order_generator.py
+++ order_generator/order_generator.py
@@ -7,7 +7,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "database": os.getenv("DB_NAME", "postgres"),
     "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", ),
+    "password": os.getenv("DB_PASSWORD"),
     "port": int(os.getenv("DB_PORT", 5432)),
 }
 
